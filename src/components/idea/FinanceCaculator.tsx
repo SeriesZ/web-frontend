78feffeabc12e0c9cdfd5f6c19cf9163435cfe9f
munjeong <ctj123@naver.com>
2025-01-09 00:15:55 +0900

    FIX 매출계획표 가로스크롤 문제로 원복

--- src/components/idea/FinanceCaculator.tsx
+++ src/components/idea/FinanceCaculator.tsx
@@ -1,11 +1,6 @@
 import {} from "react";
 import styled from "@/components/idea/Idea.module.scss";
 import { YearData } from "@/model/financeType";
-import dynamic from "next/dynamic";
-
-const ScrollHorizontal = dynamic(() => import("react-scroll-horizontal"), {
-  ssr: false, // SSR 비활성화
-});
 
 interface Props {
   itemData: {
@@ -17,166 +12,150 @@ interface Props {
 // [매출계획표(1년~10년차)]
 const FinanceCaculator: React.FC<Props> = ({ itemData }) => {
   const { plan, positiveYear } = itemData;
-  document.addEventListener("DOMContentLoaded", () => {
-    const tableContainer = document.querySelector(".xScroll");
 
-    if (tableContainer) {
-      tableContainer.addEventListener("wheel", (event: Event) => {
-        const wheelEvent = event as WheelEvent; // 확장된 타입으로 변환
-        wheelEvent.preventDefault();
-        tableContainer.scrollLeft += wheelEvent.deltaY; // deltaY 사용 가능
-      });
-    }
-  });
   return (
-    <div className={styled.xScrollContainer}>
-      <ScrollHorizontal className={styled.xScroll}>
-        <div style={{ display: "inline-block", minWidth: "100%" }}>
-          <table className={styled.years10}>
-            <thead>
-              <tr>
-                <th colSpan={2}>구분</th>
-                {plan.map((_, index) => (
-                  <th key={index}>{index + 1}년차</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>USER</th>
-                <th>거래 발생 수</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(yearData.transactionCount).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>수익</th>
-                <th>매출</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>{yearData.sales.toLocaleString()}</td>
-                ))}
-              </tr>
-              <tr>
-                <th>원가</th>
-                <th>매출원가</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>{yearData.salesCost.toLocaleString()}</td>
-                ))}
-              </tr>
-              <tr>
-                <th colSpan={2} className={styled.total}>
-                  매출총이익
+    <div className={styled.xScroll}>
+      <div style={{ display: "inline-block", minWidth: "100%" }}>
+        <table className={styled.years10}>
+          <thead>
+            <tr>
+              <th colSpan={2}>구분</th>
+              {plan.map((_, index) => (
+                <th key={index}>{index + 1}년차</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>USER</th>
+              <th>거래 발생 수</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(yearData.transactionCount).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>수익</th>
+              <th>매출</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>{yearData.sales.toLocaleString()}</td>
+              ))}
+            </tr>
+            <tr>
+              <th>원가</th>
+              <th>매출원가</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>{yearData.salesCost.toLocaleString()}</td>
+              ))}
+            </tr>
+            <tr>
+              <th colSpan={2} className={styled.total}>
+                매출총이익
+              </th>
+              {plan.map((yearData, index) => (
+                <th className={styled.total} key={index}>
+                  {yearData.grossProfit.toLocaleString()}
                 </th>
-                {plan.map((yearData, index) => (
-                  <th className={styled.total} key={index}>
-                    {yearData.grossProfit.toLocaleString()}
-                  </th>
-                ))}
-              </tr>
-              <tr>
-                <th rowSpan={8}>판관비</th>
-                <th>직원 수</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(yearData.staffCount.toFixed(0)).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>급여</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(yearData.salary.toFixed(0)).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>업무추진비</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(
-                      yearData.businessPromotionCost.toFixed(0)
-                    ).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>사무실 임차료</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(yearData.officeRent.toFixed(0)).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>접대비</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(
-                      yearData.entertainmentExpenses.toFixed(0)
-                    ).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>광고선전비</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(
-                      yearData.advertisingCost.toFixed(0)
-                    ).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>예비비</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(
-                      yearData.contingencyExpenses.toFixed(0)
-                    ).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th>판관비 계</th>
-                {plan.map((yearData, index) => (
-                  <td key={index}>
-                    {Number(yearData.adminExpenses.toFixed(0)).toLocaleString()}
-                  </td>
-                ))}
-              </tr>
-              <tr>
-                <th colSpan={2} className={styled.total}>
-                  영업이익
+              ))}
+            </tr>
+            <tr>
+              <th rowSpan={8}>판관비</th>
+              <th>직원 수</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(yearData.staffCount.toFixed(0)).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>급여</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(yearData.salary.toFixed(0)).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>업무추진비</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(
+                    yearData.businessPromotionCost.toFixed(0)
+                  ).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>사무실 임차료</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(yearData.officeRent.toFixed(0)).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>접대비</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(
+                    yearData.entertainmentExpenses.toFixed(0)
+                  ).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>광고선전비</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(yearData.advertisingCost.toFixed(0)).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>예비비</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(
+                    yearData.contingencyExpenses.toFixed(0)
+                  ).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th>판관비 계</th>
+              {plan.map((yearData, index) => (
+                <td key={index}>
+                  {Number(yearData.adminExpenses.toFixed(0)).toLocaleString()}
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <th colSpan={2} className={styled.total}>
+                영업이익
+              </th>
+              {plan.map((yearData, index) => (
+                <th className={styled.total} key={index}>
+                  {Number(yearData.operatingIncome.toFixed(0)).toLocaleString()}
                 </th>
-                {plan.map((yearData, index) => (
-                  <th className={styled.total} key={index}>
-                    {Number(
-                      yearData.operatingIncome.toFixed(0)
-                    ).toLocaleString()}
-                  </th>
-                ))}
-              </tr>
-              <tr>
-                <th colSpan={2} className={styled.total}>
-                  영업이익률
+              ))}
+            </tr>
+            <tr>
+              <th colSpan={2} className={styled.total}>
+                영업이익률
+              </th>
+              {plan.map((yearData, index) => (
+                <th className={styled.total} key={index}>
+                  {Number(
+                    (yearData.operatingIncomeRate || 0).toFixed(0)
+                  ).toLocaleString()}
+                  %
                 </th>
-                {plan.map((yearData, index) => (
-                  <th className={styled.total} key={index}>
-                    {Number(
-                      (yearData.operatingIncomeRate || 0).toFixed(0)
-                    ).toLocaleString()}
-                    %
-                  </th>
-                ))}
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </ScrollHorizontal>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
